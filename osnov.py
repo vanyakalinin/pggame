5b4a334519db70eb57f0182b6425e8c73d8885e8
vanyakalinin <166290469+vanyakalinin@users.noreply.github.com>
2024-04-07 15:27:14 +0500

    Update osnov.py

--- osnov.py
+++ osnov.py
@@ -61,6 +61,30 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+        if e.type == KEYDOWN:
+            if e.key==K_RIGHT:
+                xforce = 5
+            if e.key==K_LEFT:
+                xforce = -5
+        if e.type==KEYUP:
+            if e.key==K_RIGHT:
+                xforce = 0
+            if e.key==K_LEFT:
+                xforce = 0
+        if e.type == KEYDOWN:
+            if e.key==K_UP:
+                yforce = -5
+            if e.key==K_DOWN:
+                yforce = 5
+        if e.type==KEYUP:
+            if e.key==K_UP:
+                yforce = 0
+            if e.key==K_DOWN:
+                yforce = 0
+        if e.type == MOUSEBUTTONDOWN:
+            if e.button == 1:
+                mx = e.pos[0]
+                my = e.pos[1]
     fps.tick(60)  
     display.update() 
 
