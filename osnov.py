0a0dc12466f98d6e6059fd6ead88f79f96d391ad
vanyakalinin <166290469+vanyakalinin@users.noreply.github.com>
2024-04-07 15:12:46 +0500

    Update osnov.py
    osnova

--- osnov.py
+++ osnov.py
@@ -1,1 +1,11 @@
+from pygame import *
+from random import *
 
+okno = display.set_mode((1200,600))
+ 
+fps = time.Clock() #контроль fps
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
